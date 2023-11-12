41dc6b9a3ebf2eef201acdd7668e53a929c2d657
obaadz <obada347@gmail.com>
2023-11-12 03:30:08 +0200

    Change database name

--- src/utils/connectMongo.js
+++ src/utils/connectMongo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async () => {
   try {
     await mongoose.connect(process.env.DB_URI, {
-      dbName: "kallos",
+      dbName: "mega",
     });
 
     console.log("Connected successfully to the database");
